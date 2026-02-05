c8aee23f315031a3ebecaff41a4304fe3472f637
makasinod <makasin.od@gmail.com>
2026-02-05 12:27:11 +0200

    add error catching

--- src/httpClient.ts
+++ src/httpClient.ts
@@ -14,24 +14,34 @@ export class HttpClient {
 
 	public async get<TResponse>(endpoint: string, params = {}) {
 		const url = `${this.baseUrl}${endpoint}`;
-		const response = await axios.get<TResponse>(url, {
-			params,
-			auth: {
-				username: this.username,
-				password: this.token,
-			},
-		});
-		return response.data;
+		try {
+			const response = await axios.get<TResponse>(url, {
+				params,
+				auth: {
+					username: this.username,
+					password: this.token,
+				},
+			});
+			return response.data;
+		} catch (error) {
+			console.error("Error during GET request:", error);
+			throw error;
+		}
 	}
 
 	public async post<TResponse, TRequest>(endpoint: string, data: TRequest) {
 		const url = `${this.baseUrl}${endpoint}`;
-		const response = await axios.post<TResponse>(url, data, {
-			auth: {
-				username: this.username,
-				password: this.token,
-			},
-		});
-		return response.data;
+		try {
+			const response = await axios.post<TResponse>(url, data, {
+				auth: {
+					username: this.username,
+					password: this.token,
+				},
+			});
+			return response.data;
+		} catch (error) {
+			console.error("Error during POST request:", error);
+			throw error;
+		}
 	}
 }
